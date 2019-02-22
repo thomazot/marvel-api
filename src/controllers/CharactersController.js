38c6d91ab34d8610d88213b2d455b61e34d13315
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-02-21 22:38:51 -0300

    Remove all domains in json

--- src/controllers/CharactersController.js
+++ src/controllers/CharactersController.js
@@ -7,7 +7,8 @@ export default {
         data = Object.assign( data, api.defaults.params);
         
         const request = await api.get('/v1/public/characters', { params: data });
-        const requestData = JSON.stringify(request.data).replace(/http:\/\/gateway\.marvel\.com\/v1\/public/, '');
+        const requestData = JSON.stringify(request.data).replace(/http:\/\/gateway\.marvel\.com\/v1\/public/gi, '');
+
         res.json(JSON.parse(requestData));
     }
 }
