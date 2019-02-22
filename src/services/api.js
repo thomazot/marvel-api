9cb193ce01e6199403c22f583abd76e9bde7aca5
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-02-21 22:00:08 -0300

    Reconfigure api

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import crypto from 'crypto';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const ts         = process.env.TS;
 const privateKey = process.env.PRIVATE_KEY;
-const apikey     = process.env.PUBLIC_KEY;
+const apikey     = process.env.PUBLIC_KEY; console.log(process.env.TS);
 const hash       = crypto.createHash('md5').update(ts + privateKey + apikey).digest('hex');
 
 export default axios.create({
