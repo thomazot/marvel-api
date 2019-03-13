d0b04adf01578d6d8f3290a82be722b702d3a04b
Lucas Correia <lucascorreia-.-@live.com>
2019-03-12 21:11:39 -0300

    single route

--- src/controllers/SingleController.js
+++ src/controllers/SingleController.js
@@ -3,7 +3,7 @@ import api from '../services/api';
 export default {
     async get(req, res) {
         
-        const request = await api.get(`/v1/public/${req.query.route}?${req.query.params}=${req.query.value}`);
+        const request = await api.get(`/v1/public/${req.query.route}/${req.query.id}`);
         console.log(request)
         const requestData = JSON.stringify(request.data).replace(/http(s|):\/\/gateway\.marvel\.com\/v1\/public/gi, '');
 
