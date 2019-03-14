ff4598703b0a811aa72a369123a6dcd3f9243b26
Lucas Correia <lucascorreia-.-@live.com>
2019-03-13 21:00:23 -0300

    console log

--- src/controllers/CharactersController.js
+++ src/controllers/CharactersController.js
@@ -4,7 +4,7 @@ export default {
     async get(req, res) {
         
         const request = await api.get(`/v1/public/${req.query.route}?${req.query.params}=${req.query.value}`);
-        console.log(request)
+        
         const requestData = JSON.stringify(request.data).replace(/http(s|):\/\/gateway\.marvel\.com\/v1\/public/gi, '');
 
         res.json(JSON.parse(requestData));
--- src/controllers/SingleController.js
+++ src/controllers/SingleController.js
@@ -2,9 +2,9 @@ import api from '../services/api';
 
 export default {
     async get(req, res) {
-        
+
         const request = await api.get(`/v1/public/${req.query.route}/${req.query.id}`);
-        console.log(request)
+        
         const requestData = JSON.stringify(request.data).replace(/http(s|):\/\/gateway\.marvel\.com\/v1\/public/gi, '');
 
         res.json(JSON.parse(requestData));
